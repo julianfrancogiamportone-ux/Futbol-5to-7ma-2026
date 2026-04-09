c4742f09a46294bb86269c4b32550df8b0c3a6ef
GabiMDZA <luisgabrielaltamirano2008@gmail.com>
2026-04-08 23:39:40 -0300

    hola :v

---
+++ src/test/java/com/ejemplo/escuela/EscuelaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ejemplo.escuela;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EscuelaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
